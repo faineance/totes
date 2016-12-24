c66ea21fb640db3db7f6e6b1a51230a9683390db
faineance <faineance@users.noreply.github.com>
2016-12-24 23:26:36 +0000

    totally tbc

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# totes
+# totes ( name tbc )
+A total dependently typed functional programming language
+
+```
+bool : data
+bool = ( a : data ) -> a -> a -> a
+
+true : bool
+true = (\x y z -> y)
+
+false : bool
+false = (\x y z -> y)
+
+if : bool -> ( a : data ) -> a -> a -> a
+if = (\b -> b)
+
+
+
+```
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,28 @@
+{-# LANGUAGE DeriveDataTypeable    #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Main where
+import qualified Data.Map                         as Map
+import           Data.Typeable                    (Typeable)
+import           GHC.Generics
+import           Unbound.Generics.LocallyNameless
+
+data Expr = Data
+          | Codata
+          | Var (Name Expr)
+          | Lambda (Bind (Name Expr) Expr)
+          | Pi (Bind (Name Expr) Expr) -- "forall"
+          | App Expr Expr
+        deriving (Show, Generic, Typeable)
+
+instance Alpha Expr
+
+
+instance Subst Expr Expr where
+    isvar (Var x) = Just ( SubstName x)
+    isvar _       = Nothing
+
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = putStrLn "hello world"
