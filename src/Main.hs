5376dc5b181ca8b86f6d9fe788dc7023065cc059
faineance <faineance@users.noreply.github.com>
2016-12-25 00:10:18 +0000

    hello repl

--- src/Main.hs
+++ src/Main.hs
@@ -3,26 +3,46 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 module Main where
+import           Control.Monad.Trans
 import qualified Data.Map                         as Map
 import           Data.Typeable                    (Typeable)
 import           GHC.Generics
+import           System.Console.Haskeline
 import           Unbound.Generics.LocallyNameless
 
-data Expr = Data
-          | Codata
-          | Var (Name Expr)
-          | Lambda (Bind (Name Expr) Expr)
-          | Pi (Bind (Name Expr) Expr) -- "forall"
-          | App Expr Expr
+
+data Term = Data -- the type of types ( finite )
+          | Codata -- the type of types ( infinite (corecursive) )
+          | Var (Name Term)
+          | Lambda (Bind (Name Term) Term)
+          | Pi (Bind (Name Term) Term) -- "forall"
+          | App Term Term
         deriving (Show, Generic, Typeable)
 
-instance Alpha Expr
+instance Alpha Term
 
 
-instance Subst Expr Expr where
+instance Subst Term Term where
     isvar (Var x) = Just ( SubstName x)
     isvar _       = Nothing
 
 
+
+
+type Repl a = InputT IO a
+
+settings :: Settings IO
+settings = defaultSettings { historyFile  = Just "totes" }
+
+process :: String -> IO ()
+process = putStrLn
+
+repl :: Repl ()
+repl = do
+    minput <- getInputLine "> "
+    case minput of
+        Nothing    -> outputStrLn "Exiting"
+        Just input -> liftIO (process input) >> repl
+
 main :: IO ()
-main = putStrLn "hello world"
+main = runInputT settings repl
